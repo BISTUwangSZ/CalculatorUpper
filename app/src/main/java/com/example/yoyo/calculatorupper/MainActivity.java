ca1532f2b35d123a288d92c2dbdff70f4b38bd87
BISTUWangSZ <1028736081@qq.com>
2016-10-25 23:03:01 +0800

    final one, the code has been reorganized.

--- app/src/main/java/com/example/yoyo/calculatorupper/MainActivity.java
+++ app/src/main/java/com/example/yoyo/calculatorupper/MainActivity.java
@@ -1,11 +1,16 @@
 package com.example.yoyo.calculatorupper;
 
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     Button bmi,danwei,jinzhi;
@@ -40,6 +45,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
                 startActivity(jinzhiIntent);
             }
         });
+    }
+    //选项菜单
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        switch (id){
+            case R.id.help:
+                AlertDialog.Builder builder = new AlertDialog.Builder(MainActivity.this);
+                builder.setTitle("帮助");
+                builder.setMessage("这是一个简单的帮助");
+                builder.setNegativeButton("取消", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this,"单击了取消",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.setPositiveButton("确定", new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        Toast.makeText(MainActivity.this,"单击了确定",Toast.LENGTH_SHORT).show();
+                    }
+                });
+                builder.create();
+                builder.show();
+                break;
+            case R.id.exit:
+                System.exit(-1);
+                break;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
     public void init() {
